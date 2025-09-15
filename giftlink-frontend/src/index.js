10812564c761a1470b2407b1bfb68a5f08433b71
Atlegang Sethono <atlegang.sethono@umuzi.org>
2025-09-15 11:45:51 +0200

    Fix import

--- giftlink-frontend/src/index.js
+++ giftlink-frontend/src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
-import App from './App';
-import {AuthProvider} from './context/AuthContext';
+import App from './App.js';
+import {AuthProvider} from './context/AuthContext.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
